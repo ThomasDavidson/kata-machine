e24b3a026d4673a0093ba7164e5dbb7afd4a74f8
Thomas Davidson <thomasdavidson268@gmail.com>
2025-07-01 17:17:10 -0400

    add LRU solution

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,111 @@
+import LinkedList from "@code/DoublyLinkedList";
+
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+    prev?: Node<T>,
+}
+function createNode<V>(value: V): Node<v> {
+    return { value }
+}
+
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookup: Map<K, Node<V>>;
+    private reverse_lookup: Map<Node<V>, K>;
 
-    constructor() {
+
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>;
+        this.reverse_lookup = new Map<Node<V>, K>;
+
     }
 
     update(key: K, value: V): void {
+        // does it exist
+        let node = this.lookup.get(key);
+        if (!node) {
+            node = createNode(value);
+            this.length++;
+            this.prepend(node);
+            this.trimCache();
 
-}
+            // otherwise insert
+            this.lookup.set(key, node);
+            this.reverse_lookup.set(node, key);
+        } else {
+            // move to front
+            this.detach(node);
+            this.prepend(node);
+            // update value
+            node.value = value;
+        }
+
+
+    }
     get(key: K): V | undefined {
 
-}
+        // check the cache for existance
+        const node = this.lookup.get(key);
+        if (!node) {
+            return undefined;
+        }
+
+        // update the value we found
+        this.detach(node);
+        this.prepend(node);
+
+        // return value or undefined
+        return node.value
+    }
+
+    private detach(node: Node<V>) {
+        if (node.prev) {
+            node.prev.next = node.next;
+        }
+        if (node.next) {
+            node.next.prev = node.prev;
+        }
+
+        if (this.head === node) {
+            this.head = this.head.next;
+        }
+        if (this.tail === node) {
+            this.tail = this.tail.prev;
+        }
+
+        node.next = node.prev = undefined;
+    }
+
+    private prepend(node: Node<V>) {
+        if (!this.head) {
+            this.head = this.tail = node;
+        } else {
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+    }
+    private trimCache() {
+
+        if (this.capacity >= this.length) {
+            return
+        }
+
+        const tail = this.tail as Node<V>;
+        this.detach(tail)
+
+        const key = this.reverse_lookup.get(tail) as K
+
+        this.lookup.delete(key)
+        this.reverse_lookup.delete(tail)
+
+        this.length--;
+    }
+
 }
